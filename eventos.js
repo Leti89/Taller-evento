15e89e6fd8ace3a396f023acf7b50af95bfdebdb
Leti <ydna.andy74@gmail.com>
2024-11-11 14:59:07 -0300

    Botón y evento configurados correctamente

--- eventos.js
+++ eventos.js
@@ -1,3 +1,11 @@
+// Manejador de eventos para el contenedor (div)
 document.getElementById("contenedor").addEventListener("click", function() {
     alert("Hola! Soy el div");
   });
+  
+  // Manejador de eventos para el botón, evitando la propagación al contenedor
+  document.getElementById("saludarBtn").addEventListener("click", function(event) {
+    event.stopPropagation(); // Detiene la propagación del evento
+    alert("Hola!");
+  });
+  
